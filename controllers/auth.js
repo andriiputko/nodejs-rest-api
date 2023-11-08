def6786dd5517957e71a0cfffb87987d8c17e157
Andrii <andriiputko@gmail.com>
2023-11-08 22:33:30 +0100

    Avatar commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,73 +1,89 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const {User} = require("../models/user");
-const { ctrlWrapper, HttpError } = require('../helpers');
-const {SECRET_KEY} = process.env;
+const { User } = require("../models/user");
+const { ctrlWrapper, HttpError } = require("../helpers");
+const { SECRET_KEY } = process.env;
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-const register = async(req, res)=> {
-    const {email, password} = req.body;
-    const user = await User.findOne({email}); 
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
 
-    if (user) {
-        throw HttpError(409, "Email already in use");
-    }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-    const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
-    const newUser = await User.create({...req.body, password: hashPassword});
+  res.status(201).json({
+    email: newUser.email,
+    name: newUser.name,
+  });
+};
 
-    res.status(201).json({
-        email: newUser.email,
-        name: newUser.name,
-    })
-}
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
 
+  const payload = {
+    id: user._id,
+  };
 
-const login = async(req, res)=> {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user){
-        throw HttpError(401, "Email or password invalid");
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password invalid");
-    }
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+  });
+};
 
-    const payload = {
-        id: user._id,
-    }
+const getCurrent = async (req, res) => {
+  const { email, name } = req.user;
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-    await User.findByIdAndUpdate(user._id, {token});
-    res.json({
-        token,
-    })
-}
+  res.json({
+    email,
+    name,
+  });
+};
 
-const getCurrent = async(req, res)=> {
-    const{email, name} = req.user;
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-    res.json({
-        email,
-        name,
-    })
-}
+  res.json({
+    message: "Logout success",
+  });
+};
 
-const logout = async(req, res) => {
-    const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token: ""});
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const resultUpload = path.join(avatarsDir, originalname);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", originalname);
+  await User.findByIdAndUpdate(_id, { avatarURL });
 
-    res.json({
-        message: "Logout success"
-    })
-}
-
+  res.json({
+    avatarURL,
+  });
+};
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
-}
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
+};
